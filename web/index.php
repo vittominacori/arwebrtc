642ddaf602556195d36838175e27c595b43e6a6d
Vittorio Minacori <vittoriominacori@gmail.com>
2015-03-10 00:25:37 +0100

    First Commit

--- web/index.php
+++ web/index.php
@@ -5,7 +5,26 @@
 </head>
 <body ng-controller="CanvasCtrl">
 
-<?php include "partials/canvas.php"; ?>
+<div ng-show="!recording">
+<img src="uploads/<?=!empty($_GET['img']) ? $_GET['img'].'.png' : 'demo.png' ?>">
+	<div class="buttons">
+		<button ng-click="init()"><i class="icono-image"></i> Take yours</button>
+	</div>
+</div>
+
+<div ng-show="recording">
+
+	<video autoplay class="preview"></video>
+	<canvas height="426" width="515"></canvas>
+
+
+	<div class="buttons">
+		<button ng-click="share.save()"><i class="icono-image"></i> Save image</button>
+		<button class="fb" ng-click="share.fb()"><i class="icono-facebook"></i> Share</button>
+		<button class="tw" ng-click="share.tweet()"><i class="icono-twitter"></i> Tweet</button>
+		<button class="gp" ng-click="share.gplus()"><i class="icono-gplus"></i> +1</button>
+	</div>
+</div>
 
 <script src="js/plugins.js"></script>
 <script src="js/script.js"></script>
