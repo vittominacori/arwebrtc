7b2ed7d312c2cb16d2ae08ec3c8da38cd70db7bb
Vittorio Minacori <vittoriominacori@gmail.com>
2015-03-10 01:24:17 +0100

    metadata

--- web/app/partials/metadata.php
+++ web/app/partials/metadata.php
@@ -1,8 +1,8 @@
 <meta name="description" content="Sample of using Augmented Reality with your browser">
 
-<meta property="og:image" content="<?=$_SERVER[HTTP_HOST].$_SERVER[SCRIPT_NAME]?>uploads/<?=!empty($_GET['img']) ? $_GET['img'].'.png' : 'demo.png' ?>" />
+<meta property="og:image" content="https://<?=$_SERVER[HTTP_HOST].str_replace('index.php', '', $_SERVER[SCRIPT_NAME])?>uploads/<?=!empty($_GET['img']) ? $_GET['img'].'.png' : 'demo.png' ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:title" content="Augmented Reality with WebRTC" />
-<meta property="og:url" content="<?='http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI]?>" />
+<meta property="og:url" content="<?='https://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI]?>" />
 
 <title>Augmented Reality with WebRTC</title>
