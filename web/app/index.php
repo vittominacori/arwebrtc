057ffb9a6c6d2f3ff36de83cafcfb4014c72caf8
Vittorio Minacori <vittoriominacori@gmail.com>
2015-03-10 01:02:06 +0100

    metadata

--- web/app/index.php
+++ web/app/index.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="en" ng-app="ARWebRTC">
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<?php include "partials/metadata.php"; ?>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body ng-controller="CanvasCtrl">
 
-<?php include "partials/canvas.php"; ?>
+	<?php include "partials/canvas.php"; ?>
 
-<script src="js/plugins.js"></script>
-<script src="js/script.js"></script>
+	<script src="js/plugins.js"></script>
+	<script src="js/script.js"></script>
 </body>
 </html>
--- web/app/partials/metadata.php
+++ web/app/partials/metadata.php
@@ -1,1 +1,9 @@
-metadata.php
+<meta name="description" content="Sample of using Augmented Reality with your browser">
+
+<meta property="og:image" content="uploads/<?=!empty($_GET['img']) ? $_GET['img'].'.png' : 'demo.png' ?>" />
+<meta property="og:locale" content="en" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Augmented Reality with WebRTC" />
+<meta property="og:url" content="<?='http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI]?>" />
+
+<title>Augmented Reality with WebRTC</title>
